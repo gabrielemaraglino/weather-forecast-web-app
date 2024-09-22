44c44d3e049ff4cea414aae2e91be8fb5338acfe
gabrielemaraglino <gabrielemaraglino.04@gmail.com>
2024-09-22 11:30:44 +0200

    Add get_data function to filter data based on input request

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import streamlit as st
+import plotly.express as px
+from backend import get_data
 
 st.title("Weather Forecast for the next days")
 place = st.text_input("Place: ")
@@ -7,3 +9,8 @@ days  = st.slider("Forecast Days", min_value=1, max_value=5,
 option = st.selectbox("Select data to view",
                       ("Temperature", "Sky"))
 st.subheader(f"Temperature for the next {days} days in {place}")
+
+data = get_data(place, days, option)
+
+figure = px.line(x=d, y=t, labels={"x": "Date", "y": "Temperature (C)"},)
+st.plotly_chart(figure)
